0680da819244fb552078f477fb228d6b7a0c5254
lendackya <lendackya@gmail.com>
2016-11-05 18:37:25 -0400

    Better Printing Layout

--- TestHitOrMiss.c
+++ TestHitOrMiss.c
@@ -30,10 +30,10 @@ int main(int argc, char *argv[]) {
 		tag2cmp = 0; 
 		h = hit_or_miss(cache, 0, 0); 
 		
-	// Test 4: should return a hit
+	// Test 4: should return a miss
 	
 		index = 0; 
-		tag2cmp = 4; 
+		tag2cmp = 100; 
 		h = hit_or_miss(cache, index, tag2cmp); 
 
 	// Test 5: Should be a hit
